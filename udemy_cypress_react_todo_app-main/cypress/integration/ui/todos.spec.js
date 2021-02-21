3843fba012dafbcbae8506241679cc736f80680d
iromashko <iromashko@me.com>
2021-02-22 01:02:01 +0300

    part 2

--- udemy_cypress_react_todo_app-main/cypress/integration/ui/todos.spec.js
+++ udemy_cypress_react_todo_app-main/cypress/integration/ui/todos.spec.js
@@ -5,9 +5,20 @@ describe('Todo UI testing', () => {
     cy.visit('/');
   });
 
+  it('should be able to toggle the status of todo', () => {
+    cy.addNewTodo('First Todo');
+    
+    cy.get('.todo-checkbox').should('not.be.checked');
+    cy.get('.todo-checkbox').check().should('be.checked');
+    cy.get('.todo-checkbox').uncheck().should('not.be.checked');
+  });
+  
   it('should add a new todo correctly', () => {
-    cy.get('.todo-input').type('First Todo{enter}');
-    cy.get('.success').should('be.visible');
-    cy.get('.todo-item').last().should('contain.text', 'First Todo');
+    cy.addNewTodo('Second Todo');
+    cy.get('.todo-item').last().should('contain.text', 'Second Todo');
+  }); 
+  
+  afterEach(() => {
+    cy.get('.delete-item').click({ multiple: true });
   });
 });
--- udemy_cypress_react_todo_app-main/cypress/support/commands.js
+++ udemy_cypress_react_todo_app-main/cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('addNewTodo', (title) => {
+  cy.get('.todo-input').type(`${title}{enter}`);
+  cy.get('.success').should('be.visible');
+});
